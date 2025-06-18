0514cac9276edb9b677836b5f7195766fd7ad38f
Kalen Thomas Jr <kalenthomasjr@Kalens-MacBook-Pro.local>
2025-06-18 12:20:55 -0700

    Lab 2 completion

---
+++ lab2.py
@@ -0,0 +1,32 @@
+# Step 1: Name Variable
+name = "Kalen Thomas" 
+
+# Step 2: Nickname Variable
+nickname = "Jrsmash"
+
+# Step 3: Age Variable
+age = 15
+
+# Step 4: Boolean Variable
+# (true or False)
+used_before = False
+
+# Step 5: List Data Structure
+Hobbies = ["sleeping","Making burgers","code", "playing videogames"] # list of strings
+
+# Step 6:
+# (key : value) pairs
+my_favs = {'food': 'burgers',  'hot  dogs' 'salmon' 'movies':'marvel'}
+
+# Step 7: Set Variable
+teachers = ("teju", "parkerb", "apple", "mia",)
+
+# Step 8: Tuple Variable
+teachers1 = ("teju", "parkerb", "apple", "mia",)
+
+print(name)
+print(age)
+print(Hobbies)
+print(my_favs)
+print(teachers)
+print(teachers1) 
