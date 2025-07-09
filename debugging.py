d646ce66319f0db753eafc4cce860d142647744b
Kalen714 <kalenthomasjr@gmail.com>
2025-07-09 11:25:19 -0700

    Add files via upload

--- debugging.py
+++ debugging.py
@@ -1,1 +1,27 @@
-debugging.py completion
+# Code Snippet 1
+x = 10
+y = 0
+result = x / y
+print("Result:", result)
+# Code Snippet 2
+numbers = [1, 2, 3, 4, 5]
+for i in range(len(numbers)):
+   print(numbers[i+1])
+# Code Snippet 3
+def calculate_area(radius):
+   area = 3.14 * radius ** 2
+   return area
+radius = 5
+print(calculate_area(radius))
+# Code Snippet 4
+def is_even(number):
+   if number % (2 == 0):
+       return True
+   else:
+       return False
+ 
+print(is_even(4))
+print(is_even(7))
+
+ 
+
