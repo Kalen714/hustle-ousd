d2f417c97e93ff867b76003553b89f1e83065339
Kalen714 <kalenthomasjr@gmail.com>
2025-07-28 13:07:55 -0700

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -8,16 +8,17 @@ print(food_list)
 joined_food = ' '.join(food_list)
 print(joined_food)
 # Task 2: Working with Lists
-number_list = [1, 7675647456, 32, -5, 0, 234,]
+number_list = [1, 7675647456, 32, -5, 0, 234]
 number_list.append(67)
 # print(number_list)
 number_list.insert(3,1000)
 # print(number_list)
-number_list.pop(-5)
+number_list.pop()
 print(number_list)
 number_list.remove(7675647456)
 print(number_list)
-
+print(number_list[0:3])
+print(number_list[-3:]) 
 # Task 3: Dictionaries (key : value)
 books = {'Dr Seuss' :'How the Grinch Stole Christmas!','William  Shakespere':'Romeo and Juliet','Stan Lee' :'Marvel Comics', 'Hidenori Kusaka': 'Pokemon Adventures'}
 keys_list = ('Dr Seuss','William Shakespere')
@@ -25,7 +26,10 @@ print(keys_list)
 values = ('How the Grinch stole Christmas!','Romeo and Juliet','Marvel Comics','Pokemon Adventures')
 print(values)
 get = ('Romeo and Juliet')
+get = ('Marvel Comics')
 print(get)
 pop = ('How the Grinch stole Christmas')
+pop = ('Pokemon Adventures')
 print(pop)
-# del('Dr Seuss')
+del books['Dr Seuss']
+
