3da5df909cd02fd0f53e516e0a496ebe1d452646
Kalen714 <kalenthomasjr@gmail.com>
2025-07-30 11:44:20 -0700

    Add files via upload

---
+++ functions_activity.py
@@ -0,0 +1,11 @@
+menu = {'Pizza': 2.99, 'Burger': 3.99, 'Hot dog': 1.99, 'Cheese': 0.59, 'Ice cream': 1.49, 'Churro': 0.79, 'Soda': 0.89}
+def total_price (item1,item2):
+    menu['Pizza']+menu['Hot dog']
+    item1 = menu ['Pizza']
+    item2 = menu['Hot dog']
+print(total_price)
+def price_difference (item3,item4):
+    menu['Cheese']+['Ice cream']
+    item3 = menu['Cheese']
+    item4 = menu['Ice cream']
+print(price_difference)
